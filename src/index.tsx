e89d0a2fb4e3213c335f514a55dfc0881235e4f5
Arkadiusz Machalica <a.machalica@selleo.com>
2023-05-12 12:38:18 +0200

    Connect to asticaApi

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,10 @@ import { App } from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 
+setTimeout(() => {
+  // @ts-ignore
+  window.asticaAPI_start(import.meta.env.VITE_ASTICA_ACCESS_TOKEN)
+}, 1000)
 root.render(
   <React.StrictMode>
     <App />
