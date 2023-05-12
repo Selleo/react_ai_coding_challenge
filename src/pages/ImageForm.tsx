57a8af8dab28c3d9439a7dc80e8e58217c079ee9
Arkadiusz Machalica <a.machalica@selleo.com>
2023-05-12 12:27:28 +0200

    Implement Image form

--- src/pages/ImageForm.tsx
+++ src/pages/ImageForm.tsx
@@ -1,12 +1,47 @@
-// import { ExampleComponent } from 'components'
+import { createImage } from 'api/image/requests'
+import { useState } from 'react'
 
 export const ImageForm = () => {
+  const [imageURL, setImageURL] = useState('')
+  const [alt, setAlt] = useState('')
+  const submit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    createImage({ url: imageURL, alt })
+  }
   return (
     <div>
-      <p>
-        Implement form for uploading image with alt text as described in the
-        requirements
-      </p>
+      <form onSubmit={submit}>
+        <label htmlFor="image">Image URL</label>
+        <input
+          type="text"
+          id="image"
+          name="image"
+          onChange={({ target }) => setImageURL(target.value)}
+          value={imageURL}
+        />
+        {imageURL && (
+          <img
+            src={imageURL}
+            alt="placeholder"
+            style={{
+              maxWidth: '100%',
+              maxHeight: '500px',
+            }}
+          />
+        )}
+        {/*<input type="file" id="image" name="image" accept="image/*" />*/}
+        <label htmlFor="alt">Alt text</label>
+        <input
+          type="text"
+          id="alt"
+          name="alt"
+          value={alt}
+          onChange={({ target }) => setAlt(target.value)}
+        />
+        <br />
+        <button>Submit</button>
+      </form>
     </div>
   )
 }
